b449163de52348ff6e300c76535c74c8e6ead2d6
Krunal <krunal.soni@cognizant.com>
2016-10-12 00:07:34 +0530

    Header updated

--- Seed-GoogleCloudVision/ImageModel.m
+++ Seed-GoogleCloudVision/ImageModel.m
@@ -5,6 +5,9 @@
 //  Created by Krunal Soni on 21/09/16.
 //  Copyright © 2016 Cognizant. All rights reserved.
 //
+//
+//This file is subject to the terms and conditions defined in
+//file 'LICENSE.md', which is part of this source code package.
 
 #import "ImageModel.h"
 #import <UIKit/UIKit.h>
--- Seed-GoogleCloudVision/MainViewController.m
+++ Seed-GoogleCloudVision/MainViewController.m
@@ -5,6 +5,9 @@
 //  Created by Krunal Soni on 27/09/16.
 //  Copyright © 2016 Cognizant. All rights reserved.
 //
+//
+//This file is subject to the terms and conditions defined in
+//file 'LICENSE.md', which is part of this source code package.
 
 #import "MainViewController.h"
 #import "ImagePickerViewController.h"
